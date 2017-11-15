4fe04758eb7a2e015fd4c45af89a85c53b4f9202
Irina <33501571+Sat0ri@users.noreply.github.com>
2017-11-15 06:00:51 +0200

    Update Evolution Strategy with Neural Nets.py

--- Evolution Strategy with Neural Nets.py
+++ Evolution Strategy with Neural Nets.py
@@ -1,3 +1,9 @@
+"""
+Simple code for Distributed ES proposed by OpenAI.
+Based on this paper: Evolution Strategies as a Scalable Alternative to Reinforcement Learning
+Details can be found in : https://arxiv.org/abs/1703.03864
+Visit more on my tutorial site: https://morvanzhou.github.io/tutorials/
+"""
 import pickle
 import numpy as np
 import gym
